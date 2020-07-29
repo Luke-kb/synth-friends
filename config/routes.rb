e9b0ced0fdb024e3b53e3e31fef81838e27ed415
Luke-kb <keananbrown@gmail.com>
2020-07-29 14:13:58 +1000

    scaffolded Profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  resources :profiles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
-  root 'pages#index'
+  root 'home#page'
   devise_for :users
 
 end
