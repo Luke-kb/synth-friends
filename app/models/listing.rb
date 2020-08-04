fa34169cf5bbe38b661f41c3306cfc272066dfb1
Luke-kb <keananbrown@gmail.com>
2020-08-04 13:00:07 +1000

    setup self join relationships in listing and profiles models

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,7 @@
 class Listing < ApplicationRecord
   belongs_to :category
   belongs_to :brand
+  belongs_to :lender, class_name: "Profile"
+  belongs_to :renter, class_name: "Profile", optional: true
   has_many_attached :image
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,5 +2,8 @@ class Profile < ApplicationRecord
   belongs_to :user
   has_many :ratings
   has_one_attached :image
+  #self join setup
+  has_many :listings_to_lend, class_name: "Listing", foreign_key: "lender_id"
+  has_many :listings_to_rent, class_name: "Listing", foreign_key: "renter_id"
 
 end
