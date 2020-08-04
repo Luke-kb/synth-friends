0311e2523a92af491390d246121fb6b86912ade7
Luke-kb <keananbrown@gmail.com>
2020-08-04 15:53:37 +1000

    updated seed file to populate brands and category. added pry to gemfile

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem 'devise'
 gem 'bootstrap', '~> 4.5.0'
 gem 'jquery-rails'
 gem 'aws-sdk-s3', require: false
+gem 'pry'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+brands_array = [
+    "Arturia", "Moog", "Korg", "Elektron", "Dave Smith Instruments", "Yamaha", "Access", "Waldorf"
+]
+
+categories_array = [
+    "Keyboard Synth", "Desktop Synth", "Effects", "Midi Controller", "Modular", "Accessories"
+]
+
+brands_array.each do |brand|
+    Brand.create(name: brand)
+end
+
+categories_array.each do |category|
+    Category.create(name: category)
+end
