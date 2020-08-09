59d745e9fd790ff72fd5f241a58a9c9d5718fad5
Luke-kb <keananbrown@gmail.com>
2020-08-09 12:26:28 +1000

    small changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
-
+    
     rescue_from CanCan::AccessDenied do |exception|
         flash[:alert] = exception.message
         redirect_to root_path 
@@ -9,6 +9,5 @@ class ApplicationController < ActionController::Base
     def after_sign_in_path_for(resource_or_scope)
         profile_path(current_user) 
     end
-
 
 end
