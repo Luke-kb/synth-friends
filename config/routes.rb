ad6478f504cf666d1921917202724e99f88b2fc2
Luke-kb <keananbrown@gmail.com>
2020-08-09 14:58:46 +1000

    minor changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#page'
   devise_for :users
-  resources :loans
+  resources :users, only: [:show]
+  resources :listings
+  resources :profiles
   resources :categories
   resources :brands
-  resources :listings
+  resources :loans
   resources :ratings
-  resources :profiles
-  resources :users, only: [:show]
+  
   get 'search', to: "listings#search"
- 
 end
