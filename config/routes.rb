efca6e84686c91df22b245b9597791bf6aa3f5f6
Luke-kb <keananbrown@gmail.com>
2020-08-09 22:20:58 +1000

    added new routes and view for user_listings

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show]
   resources :listings
+  get '/listings/:id', to: 'listings#user_listings', as: 'user_listings'
   resources :profiles
   resources :categories
   resources :brands
