ac3cd5192b2e94f1c41e113b8bc9118ff6c1003f
Luke-kb <keananbrown@gmail.com>
2020-08-09 12:21:25 +1000

    cleaned up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'home#page'
+  devise_for :users
   resources :loans
   resources :categories
   resources :brands
   resources :listings
   resources :ratings
- 
-  root 'home#page'
-  devise_for :users
   resources :profiles
   resources :users, only: [:show]
   get 'search', to: "listings#search"
+ 
 end
