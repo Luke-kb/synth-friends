7f863aea477798e102d9895bc37521e52b0f0c31
Luke-kb <keananbrown@gmail.com>
2020-08-11 09:50:42 +1000

    styling and migration editing

--- db/migrate/20200804042742_drop_brands.rb
+++ db/migrate/20200804042742_drop_brands.rb
@@ -1,6 +1,10 @@
 class DropBrands < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :brands
+  def up
     drop_table :categories
+    drop_table :brands
+  end
+  
+  def down
+    fail ActiveRecord::IrreversibleMigration
   end
 end
