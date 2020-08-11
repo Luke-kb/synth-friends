b864a3db7584b8165d387a2f01d0ac57aa670461
Luke-kb <keananbrown@gmail.com>
2020-08-11 17:02:11 +1000

    clean up

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,6 +1,5 @@
 class Loan < ApplicationRecord
   before_save :calculate_total_cost 
-
   belongs_to :listing
   has_one :lender, class_name: "Profile"
   has_one :renter, class_name: "Profile"
@@ -8,5 +7,4 @@ class Loan < ApplicationRecord
   def calculate_total_cost
     self.total_cost = ((end_date.to_date - start_date.to_date) * listing.price)
   end
-
 end
