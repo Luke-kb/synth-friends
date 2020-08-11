dd5f557fd2ca07ce0ae27bbcccfa331e8cdd5b15
Luke-kb <keananbrown@gmail.com>
2020-08-11 12:12:58 +1000

    cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#page'
-  devise_for :users
+  devise_for :users 
   resources :users, only: [:show]
+  resources :profiles
   resources :listings
   get '/user_listings/:id/', to: 'listings#user_listings', as: 'user_listings'
-  resources :profiles
   resources :categories
   resources :brands
   resources :loans
